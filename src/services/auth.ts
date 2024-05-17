97e3323e8423dffd735df1003519cb8409707019
MaiAleja <maleja.fer02@gmail.com>
2024-05-17 17:22:07 -0500

    Prueba.

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import { LOGIN } from '../config/endpoints';
 import { LoginModel } from '../models/LoginModel';
+import { REGISTER } from '../config/endpoints';
 
 export const loginService = (values: LoginModel) => {
 	return axios.post(LOGIN(), values, {
 		headers: { 'Content-Type': 'application/json' },
 	});
 };
+
+
+
